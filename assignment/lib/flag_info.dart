f6bd4bc4c8716f96f607207d641dfb5185fe02ad
MD.Robiul Islam <rjlimon2002@gmail.com>
2025-05-20 07:43:29 +0600

    added_files

---
+++ assignment/lib/flag_info.dart
@@ -0,0 +1,50 @@
+class FlagInfo {
+  final String flagUrl;
+  final String country;
+  final String title;
+
+  FlagInfo({required this.flagUrl, required this.country, required this.title});
+}
+
+final List<FlagInfo> flagCards = [
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/bg-flag.gif",
+    country: "Bangladesh",
+    title: "Country Code: +880",
+  ),
+  FlagInfo(
+    flagUrl: "https://flagcdn.com/w320/br.png",
+    country: "Brazil",
+    title: "Country Code: +55",
+  ),
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/sa-flag.gif",
+    country: "Saudi Arabia",
+    title: "Country Code: +966",
+  ),
+  FlagInfo(
+    flagUrl: "https://flagcdn.com/w320/gb.png",
+    country: "United Kingdom",
+    title: "Country Code: +44",
+  ),
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/ja-flag.gif",
+    country: "Japan",
+    title: "Country Code: +81",
+  ),
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/af-flag.gif",
+    country: "Afghanistan",
+    title: "Country Code: +93",
+  ),
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/us-flag.gif",
+    country: "United States",
+    title: "Country Code: +1",
+  ),
+  FlagInfo(
+    flagUrl: "https://www.worldometers.info/img/flags/palestine-flag.gif",
+    country: "Palestine",
+    title: "Country Code: +970",
+  ),
+];
